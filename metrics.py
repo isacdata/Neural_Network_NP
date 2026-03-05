d9c56f4ded84085c1c1f8b07975595315284f2e8
Isac Vieira <isacnascimento1@outlook.com>
2026-03-05 09:43:08 -0300

    Adicionando arquivos de métricas

---
+++ metrics.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def confusion_matrix(y_true, y_pred):
+    tp = np.sum((y_true == 1) & (y_pred == 1))
+    fp = np.sum((y_true == 0) & (y_pred == 1))
+    fn = np.sum((y_true == 1) & (y_pred == 0))
+    tn = np.sum((y_true == 0) & (y_pred == 0))
+    return np.array([[tn, fp], [fn, tp]])
+
+def accuracy(y_true, y_pred):
+    correct = np.sum(y_true == y_pred)
+    total = y_true.shape[0]
+    return correct / total if total > 0 else 0
+
+def recall(y_true, y_pred):
+    tp = np.sum((y_true == 1) & (y_pred == 1))
+    fn = np.sum((y_true == 1) & (y_pred == 0))
+    return tp / (tp + fn) if (tp + fn) > 0 else 0
+
+def precision(y_true, y_pred):
+    tp = np.sum((y_true == 1) & (y_pred == 1))
+    fp = np.sum((y_true == 0) & (y_pred == 1))
+    return tp / (tp + fp) if (tp + fp) > 0 else 0
+
+def f1_score(y_true, y_pred):
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    return 2 * (p * r) / (p + r) if (p + r) > 0 else 0
